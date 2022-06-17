d959d6761f716392baf1823037dcf6c5d1835255
AntonioTadeu <49735546+AntonioTadeu@users.noreply.github.com>
2022-06-16 22:28:15 -0300

    Create Carro.java

---
+++ Command/Carro.java
@@ -0,0 +1,23 @@
+public class Carro {
+
+    private int porta;
+    private int motor;
+    private String banco;
+
+    public Carro(int porta, int banco) {
+        this.banco = banco;
+        this.motor = motor;
+    }
+
+    public String getCarro() {
+        return Carro;
+    }
+
+    public void abrirCarro() {
+        this.Carro = "Carro a venda";
+    }
+
+    public void fechaCarro() {
+        this.Carro = "Carro Comprado";
+    }
+}
